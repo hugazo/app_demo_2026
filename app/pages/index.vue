8e1b6a81124a9829caa3ac4cb28920ac7ef99504
Hugo <hmoralesg@icloud.com>
2026-01-16 03:57:30 -0300

    feat: ios local runner

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,5 +1,12 @@
 <template>
   <ion-page>
-    <p>Hi</p>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Home</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content class="ion-padding">
+      Hello World
+    </ion-content>
   </ion-page>
 </template>
