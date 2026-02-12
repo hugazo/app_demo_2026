70d1bcafc5d15d8813ff6e270771671c5ff64688
Hugo <hmoralesg@icloud.com>
2026-02-12 02:37:13 -0300

    fix: updates auth router for / route handling

--- app/middleware/00-auth.global.ts
+++ app/middleware/00-auth.global.ts
@@ -1,10 +1,13 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   if (import.meta.server) return;
-
   const router = useIonRouter();
 
   const homePath = '/tabs/home';
   const loginPath = '/login';
+
+  if (to.path === '/') {
+    return router.push(homePath);
+  }
 
   const { loggedIn, loadSession } = useAuth();
   await loadSession();
