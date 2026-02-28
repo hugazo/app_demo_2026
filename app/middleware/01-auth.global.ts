8f4083f47e4e5f510f1a3c1914ecbb8b5be0d5c2
Hugo <hmoralesg@icloud.com>
2026-02-28 19:07:25 -0300

    fix: fixes pinia warning on router load

--- app/middleware/01-auth.global.ts
+++ app/middleware/01-auth.global.ts
@@ -1,17 +1,19 @@
 export default defineNuxtRouteMiddleware(async (to, _from) => {
   // Allows Plugin to Initialize
+
   const nuxtApp = useNuxtApp();
+  const pinia = usePinia();
+
   if (
     (import.meta.client && nuxtApp.isHydrating && nuxtApp.payload.serverRendered)
     || import.meta.server
+    || pinia === undefined
   ) return;
 
   const homePath = '/tabs/home';
   const loginPath = '/login';
 
-  const { session } = authState();
-
-  const loggedIn = !!session?.token;
+  const { loggedIn } = useAuth();
 
   if (loggedIn) {
     if (
